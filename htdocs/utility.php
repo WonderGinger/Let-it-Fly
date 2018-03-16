e041d7754dba7ea873b4b125ce670fe26b5bbd1e
Wonder Ginger <brandt.willems@gmail.com>
2018-03-16 02:38:25 -0700

    added error page redirect function because we had duplicate code.

--- htdocs/utility.php
+++ htdocs/utility.php
@@ -6,4 +6,8 @@ function mysql_fix_string($connection, $string) {
     if(get_magic_quotes_gpc()) $string = stripslashes($string);
     return $connection->real_escape_string($string);
 }
+function display_error_page(){
+    header("Location: http://www.sjsu.edu/");
+    exit();
+}
 ?>
