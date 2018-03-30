22500a8ad6d45ac7bbc80b16903d3d1fed98c629
ktsng <22183062+ktsng@users.noreply.github.com>
2018-03-29 22:19:00 -0700

    Cleaned up map interface, fixed z-index interference bug, container for future request form.

--- htdocs/index.php
+++ htdocs/index.php
@@ -52,63 +52,23 @@ mysqli_close($dbh);
       <li><a class="teal-text text-lighten-1 waves-effect" href="about">User Manual</a></li>
       <li><a class="teal-text text-lighten-1 waves-effect" href="account">Account Settings</a></li>
     </ul>
-
-
-    <div style="height: calc(100% - 62px); margin-top:2px; background-color:blue;">
-      <div class="card" style="height: 100%;">
-        <div class="card-image"  style="height: calc(100% - 64px);  background-color: red;">
-        
-        <div class="container" style="position: relative;">
-      <div id="floating-panel">
-    hi
+    <!-- Content -->
+    <div class="card">
+      <div id="map"></div>
+      <div class="card-content activator">
+        <span class="card-title activator center-align teal-text lighten-1">Choose Your Location<i class="material-icons">keyboard_arrow_up</i></span>
+      </div>
+      <div class="card-reveal grey lighten-3">
+        <span class="card-title center-align teal-text lighten-1">Request a Ride<i class="material-icons">keyboard_arrow_down</i></span>
+        <div class="container">
+          <!-- TODO: add request form fields -->
+        </div>
       </div>
     </div>
-          <div id="map"></div>
-        </div>
-        
-        <div class="card-content white-text">
-          <span class="card-title activator grey-text text-darken-4 center-align">Request a Ride</span>
-        </div>
-        <div class="card-reveal">
-          <span class="card-title grey-text text-darken-4">Card Title<i class="material-icons right">close</i></span>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-          <p>Here is some more information about this product that is only revealed once clicked on.</p>
-        </div>
-      </div>
-    <div>
-
-
-    <style>
-      #map {
-        height: calc(100%);
-        max-height: 1024px;
-        width: 100%;
-      }
-    </style>
-
-
-    <script>
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 37.3352, lng: -121.8811},
-          disableDefaultUI: true,
-          zoom: 15
-        });
-      }
-    </script>
-
     <!-- Import JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-alpha.4/js/materialize.min.js"></script>
-    <script src="js/sidenav.js"></script>
-
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRIbTYik2x_e5--W85NiB2bckEMFDjVtc&callback=initMap" async defer></script>
+    <script src="js/sidenav.js"></script>
     <script src="js/map.js"></script>
   </body>
 </html>
---
+++ htdocs/js/map.js
@@ -0,0 +1,7 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 37.3352, lng: -121.8811},
+    disableDefaultUI: true,
+    zoom: 15
+  });
+}
