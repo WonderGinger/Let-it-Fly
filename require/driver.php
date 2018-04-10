1520f317cb1008e52345aeeeac1e9fdc5756341d
Wonder Ginger <brandt.willems@gmail.com>
2018-04-10 14:49:55 -0700

    DB bugs

--- require/driver.php
+++ require/driver.php
@@ -1,4 +1,4 @@
 <?php
-echo $_SESSION["user"];
-echo $_SESSION["email"];
+    if (!$result = $dbh->query("SELECT * FROM drivers")) echo "fail";
+    // $result = $result->fetch_array(MYSQLI_ASSOC);
 ?>
