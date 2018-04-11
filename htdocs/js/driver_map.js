08d725f450066bb23bb629d327c2bdc367724a15
Wonder Ginger <brandt.willems@gmail.com>
2018-04-10 21:47:25 -0700

    Progress bar when you click the START button, letting you know you're searching for a rider

--- htdocs/js/driver_map.js
+++ htdocs/js/driver_map.js
@@ -1,4 +1,4 @@
-// Start / Stop working 
+// Start / Stop "working" 
 document.getElementById("working-toggle").addEventListener("click", function(){
     let workingValue = 0;
     if(this.textContent == "END") {
@@ -7,6 +7,7 @@ document.getElementById("working-toggle").addEventListener("click", function(){
         if(!this.classList.contains("green")) 
             this.classList.add("green");
         this.textContent = "START";
+        endWorking();
         workingValue = 0;
     } else {
         if(this.classList.contains("green")) 
@@ -14,12 +15,27 @@ document.getElementById("working-toggle").addEventListener("click", function(){
         if(!this.classList.contains("red"))
             this.classList.add("red");
         this.textContent = "END";
+        startWorking();
         workingValue = 1;
     }
     $.post("js/ajax/request.php", {
         selector: "working",
         value: workingValue
     }, function(output){
-        console.log(output);
+        // console.log(output);
     });
 });
+
+function startWorking(){
+    document.getElementById("preload").classList.remove("determinate");
+    document.getElementById("preload").classList.add("indeterminate");
+    document.getElementById("waiting-message").innerHTML = "Waiting for rider request";
+    document.getElementById("progress").style.visibility = "visible";
+}
+
+function endWorking(){
+    document.getElementById("preload").classList.remove("indeterminate");
+    document.getElementById("preload").classList.add("determinate");
+    document.getElementById("waiting-message").innerHTML = "";
+    document.getElementById("progress").style.visibility = "hidden";    
+}
--- require/driver.php
+++ require/driver.php
@@ -1,22 +1,24 @@
 
 <!-- One of the two ways of doing map display logic.-->
 <div class="container">
-    <div class="section teal-text lighten-3 center-align">
+    <div class="section teal-text lighten-1 center-align">
         <div class="row">
-            <a id="working-toggle" class="col s12 waves-effect green lighten-1 waves-light btn-large">START</a>
+            <a id="working-toggle" class="col s12 waves-effect green lighten-1 waves-light btn-large progress">START</a>
         </div>
+        <div class="row">
+            <div class="progress" id="progress" style="visibility: hidden"><div class="determinate" id="preload"></div></div>  
+        </div>
+        <div class="row">
+            <p id="waiting-message"></p>
+        </div>
+
     </div>
 </div>
 
 
 <?php
-
-if (!$result = $dbh->query("SELECT * FROM drivers WHERE email='{$_SESSION['email']}' LIMIT 1")) echo $result;
-$result = $result->fetch_array(MYSQLI_ASSOC);
-$_SESSION["id"] = $result["id"];
-
-// Toggle WORKING column in database:
-// if (!$result = $dbh->query("UPDATE drivers SET working=!working WHERE id='{$_SESSION['id']}'")) db_error();
-
-
+// Add ID to session
+    if (!$result = $dbh->query("SELECT * FROM drivers WHERE email='{$_SESSION['email']}' LIMIT 1")) echo $result;
+    $result = $result->fetch_array(MYSQLI_ASSOC);
+    $_SESSION["id"] = $result["id"];
 ?>
