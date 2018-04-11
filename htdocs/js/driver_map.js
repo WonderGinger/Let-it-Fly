fe463083aa4f14157021ac278cf7dc6670552d71
Wonder Ginger <brandt.willems@gmail.com>
2018-04-10 21:55:18 -0700

    Code clean up

--- htdocs/js/driver_map.js
+++ htdocs/js/driver_map.js
@@ -2,20 +2,10 @@
 document.getElementById("working-toggle").addEventListener("click", function(){
     let workingValue = 0;
     if(this.textContent == "END") {
-        if(this.classList.contains("red"))         
-            this.classList.remove("red");
-        if(!this.classList.contains("green")) 
-            this.classList.add("green");
-        this.textContent = "START";
-        endWorking();
+        endWorking(this);
         workingValue = 0;
     } else {
-        if(this.classList.contains("green")) 
-            this.classList.remove("green");
-        if(!this.classList.contains("red"))
-            this.classList.add("red");
-        this.textContent = "END";
-        startWorking();
+        startWorking(this);
         workingValue = 1;
     }
     $.post("js/ajax/request.php", {
@@ -26,16 +16,36 @@ document.getElementById("working-toggle").addEventListener("click", function(){
     });
 });
 
-function startWorking(){
+function startWorking(element){
+    // Change button to END
+    if(element.classList.contains("green")) 
+        element.classList.remove("green");
+    if(!element.classList.contains("red"))
+        element.classList.add("red");
+    element.textContent = "END";
+
+    // Initialize the progress bar and status
     document.getElementById("preload").classList.remove("determinate");
     document.getElementById("preload").classList.add("indeterminate");
     document.getElementById("waiting-message").innerHTML = "Waiting for rider request";
     document.getElementById("progress").style.visibility = "visible";
 }
 
-function endWorking(){
+function endWorking(element){
+    // TODO: Add error checks here to made sure it's allowed for the driver to stop working
+
+    // Change button back to START
+    if(element.classList.contains("red"))         
+        element.classList.remove("red");
+    if(!element.classList.contains("green")) 
+        element.classList.add("green");
+    element.textContent = "START";
+
+    // Get rid of the progress bar if it isn't already taken care of.
     document.getElementById("preload").classList.remove("indeterminate");
     document.getElementById("preload").classList.add("determinate");
     document.getElementById("waiting-message").innerHTML = "";
     document.getElementById("progress").style.visibility = "hidden";    
+
+
 }
