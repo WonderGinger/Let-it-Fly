1c556d61fb8c318a835fbb5752e9545dd4d26ba1
ryankyawwoo <36083181+ryankyawwoo@users.noreply.github.com>
2018-04-14 11:02:59 -0700

    just cleanup some comments and white lines

--- db-clone.sql
+++ db-clone.sql
@@ -1,18 +1,17 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Apr 10, 2018 at 04:21 PM
--- Server version: 10.0.34-MariaDB-0ubuntu0.16.04.1
--- PHP Version: 7.0.28-0ubuntu0.16.04.1
+-- Host: 127.0.0.1
+-- Generation Time: Apr 13, 2018 at 04:01 AM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
 
 SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -30,7 +29,6 @@ SET time_zone = "+00:00";
 --
 
 DROP TABLE IF EXISTS `drivers`;
-
 CREATE TABLE `drivers` (
   `id` int(11) NOT NULL,
   `email` varchar(255) NOT NULL,
@@ -46,7 +44,6 @@ CREATE TABLE `drivers` (
 --
 -- Truncate table before insert `drivers`
 --
-
 TRUNCATE TABLE `drivers`;
 --
 -- Dumping data for table `drivers`
@@ -71,13 +68,11 @@ INSERT INTO `drivers` (`id`, `email`, `password`, `active`, `seats`, `working`, 
 (16, 'driver16@domain.com', '$2y$10$wsz.CkUI4VNiYZJCwqtNDetk/AYzVoasgs3RivrJSvZQnVGNLSaLW', 0, 1, 1, 1, 37.6213129, -122.3789554),
 (17, 'driver17@domain.com', '$2y$10$M3AQymsppeIcJ0YCDHl6k.9i5/z48ECm7ASbA0W0aiJxSifOUasdy', 0, 2, 1, 1, 37.7125689, -122.2197428),
 (18, 'driver18@domain.com', '$2y$10$3RuD1p3byp9MpKfZO2AaMOeM9V7RtlP1I.VhhA6FXwTrh6sYlCJYW', 0, 3, 1, 1, 37.3639472, -121.92893750000002);
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `driver_routes`
 --
-
 DROP TABLE IF EXISTS `driver_routes`;
 CREATE TABLE `driver_routes` (
   `driver` int(11) NOT NULL,
@@ -87,14 +82,12 @@ CREATE TABLE `driver_routes` (
 --
 -- Truncate table before insert `driver_routes`
 --
-
 TRUNCATE TABLE `driver_routes`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `requests`
 --
-
 DROP TABLE IF EXISTS `requests`;
 CREATE TABLE `requests` (
   `id` int(11) NOT NULL,
@@ -108,14 +101,12 @@ CREATE TABLE `requests` (
 --
 -- Truncate table before insert `requests`
 --
-
 TRUNCATE TABLE `requests`;
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `riders`
 --
-
 DROP TABLE IF EXISTS `riders`;
 CREATE TABLE `riders` (
   `id` int(11) NOT NULL,
@@ -129,22 +120,18 @@ CREATE TABLE `riders` (
 --
 -- Truncate table before insert `riders`
 --
-
 TRUNCATE TABLE `riders`;
 --
 -- Dumping data for table `riders`
 --
-
 INSERT INTO `riders` (`id`, `email`, `password`, `active`, `lat`, `lng`) VALUES
 (1, 'ken@domain.com', '$2y$10$GBo5v48sqhnPts3aoGS4M.Ff0VRyYPdn1t6BxV3Mwz3Lmi/y22kHy', 0, 37.61614, -122.38412),
 (2, 'ken1@domain.com', '$2y$10$II/7ypuFD.gVNdtOTKNzs.07D3wRYPUgCwwAqDhUNKsKXbY5TqaDq', 0, 37.61614, -122.38412);
-
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `rider_routes`
 --
-
 DROP TABLE IF EXISTS `rider_routes`;
 CREATE TABLE `rider_routes` (
   `rider` int(11) NOT NULL,
@@ -154,12 +141,10 @@ CREATE TABLE `rider_routes` (
 --
 -- Truncate table before insert `rider_routes`
 --
-
 TRUNCATE TABLE `rider_routes`;
 --
 -- Dumping data for table `rider_routes`
 --
-
 INSERT INTO `rider_routes` (`rider`, `route`) VALUES
 (1, '37.3457021`-121.94655649999999`37.6172739`-122.38422589999999`[[{"lat":37.61634,"lng":-122.38399000000001},{"lat":37.616440000000004,"lng":-122.38393},{"lat":37.61647,"lng":-122.38391000000001},{"lat":37.616490000000006,"lng":-122.38390000000001},{"lat":37.616510000000005,"lng":-122.38390000000001},{"lat":37.61654,"lng":-122.38389000000001},{"lat":37.616600000000005,"lng":-122.38389000000001},{"lat":37.616640000000004,"lng":-122.38391000000001},{"lat":37.616780000000006,"lng":-122.38397},{"lat":37.616870000000006,"lng":-122.38401},{"lat":37.61701,"lng":-122.38406},{"lat":37.61717,"lng":-122.38413000000001},{"lat":37.617180000000005,"lng":-122.38414000000002},{"lat":37.617270000000005,"lng":-122.38423000000002}]]'),
 (2, '37.3440832`-121.87393420000001`37.6172739`-122.38422589999999`[[{"lat":37.61634,"lng":-122.38399000000001},{"lat":37.616440000000004,"lng":-122.38393},{"lat":37.61647,"lng":-122.38391000000001},{"lat":37.616490000000006,"lng":-122.38390000000001},{"lat":37.616510000000005,"lng":-122.38390000000001},{"lat":37.61654,"lng":-122.38389000000001},{"lat":37.616600000000005,"lng":-122.38389000000001},{"lat":37.616640000000004,"lng":-122.38391000000001},{"lat":37.616780000000006,"lng":-122.38397},{"lat":37.616870000000006,"lng":-122.38401},{"lat":37.61701,"lng":-122.38406},{"lat":37.61717,"lng":-122.38413000000001},{"lat":37.617180000000005,"lng":-122.38414000000002},{"lat":37.617270000000005,"lng":-122.38423000000002}]]');
