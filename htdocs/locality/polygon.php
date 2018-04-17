8218357b218076c8ac7cdd43239730874175fe91
ktsng <22183062+ktsng@users.noreply.github.com>
2018-04-16 23:34:52 -0700

    clean up directories and deprecated files

--- htdocs/locality/polygon.php
+++
@@ -1,9 +0,0 @@
-<?php
-$lines = file("raw/santa-cruz.txt", FILE_IGNORE_NEW_LINES);
-for ($i = 0; $i < count($lines); $i++) {
-  $parse = explode(", ", $lines[$i]);
-  echo "new google.maps.LatLng(" . $parse[1] . ", " . $parse[0] . ")";
-  if ($i != count($lines) - 1) echo ",";
-  echo "<br>";
-}
-?>
