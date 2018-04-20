41d7b12e2f049a543891f7ebf465856f4c30d9ea
Wonder Ginger <brandt.willems@gmail.com>
2018-04-20 01:31:46 -0700

    Finished credit card entry page (needs to be integrated to the rest of the site)

--- htdocs/js/checkout.js
+++ htdocs/js/checkout.js
@@ -1,16 +1,90 @@
+var valid_card = false;
+var valid_month = false;
+var valid_year = false;
+var valid_cvc = false;
+
 $(document).ready(function() {
-    $('input#input_text, textarea#textarea2').characterCounter();
-    // var cardNumberField = document.getElementById("input_text");
-    // cardNumberField.addEventListener("change", function(event){
-    //     if(validateCardNumber(event)){
-    //         console.log("green")
-    //     }
-    //     else {
-    //         //red
-    //     }
-    // });
+    // CARD #
+    document.getElementById("card_number").addEventListener("input", function(){
+        if($(this).val().length >= 16) {
+            $(this).val(($(this).val().substr(0, 16)));
+        }
+        let validated = validateCardNumber($(this).val());
+        if(validated) { 
+            $(this).removeClass("invalid");
+            $(this).addClass("valid");
+            valid_card = true;
+        }
+        else { 
+            $(this).removeClass("valid");
+            $(this).addClass("invalid");
+            valid_card = false;
+        }
+    })
+    // MONTH
+    document.getElementById("month").addEventListener("input", function(){
+        if($(this).val().length >= 2) {
+            $(this).val(($(this).val().substr(0, 2)));
+        }
+        let validated = validateMonth($(this).val());
+        if(validated) { 
+            $(this).removeClass("invalid");
+            $(this).addClass("valid");
+            valid_month = true;
+        }
+        else { 
+            $(this).removeClass("valid");
+            $(this).addClass("invalid");
+            valid_month = false;
+        }
+    })
+    // YEAR
+    document.getElementById("year").addEventListener("input", function(){
+        if($(this).val().length >= 2) {
+            $(this).val(($(this).val().substr(0, 2)));
+        }
+        let validated = validateYear($(this).val());
+        if(validated) { 
+            $(this).removeClass("invalid");
+            $(this).addClass("valid");
+            valid_year = true;
+        }
+        else { 
+            $(this).removeClass("valid");
+            $(this).addClass("invalid");
+            valid_year = false;
+        }
+    })
+    // CVC
+    document.getElementById("cvc").addEventListener("input", function(){
+        if($(this).val().length >= 3) {
+            $(this).val(($(this).val().substr(0, 3)));
+        }
+        let validated = validateCVC($(this).val());
+        if(validated) { 
+            $(this).removeClass("invalid");
+            $(this).addClass("valid");
+            valid_cvc = true;
+        }
+        else { 
+            $(this).removeClass("valid");
+            $(this).addClass("invalid");
+            valid_cvc = false;
+        }
+    })
+
+    // SUBMIT
+    document.getElementById("submit").addEventListener("click", function(){
+        if(valid_card && valid_month && valid_year && valid_cvc) {
+            document.getElementById("form").remove();
+            document.getElementById("thanks").innerHTML = "<h3>Thank you!</h3>";
+            setTimeout(function(){
+                window.location.replace("/");
+            }, 3000);
+        }
+    })
   });
-  
+
 function validateCardNumber(number) {
     var regex = new RegExp("^[0-9]{16}$");
     if (!regex.test(number))
@@ -32,4 +106,18 @@ function luhnCheck(val) {
         sum += intVal;
     }
     return (sum % 10) == 0;
+}
+
+function validateMonth(number) {
+    return (number - 0) == number && number <=12 && number > 0 && number.length > 1;
+}
+function validateYear(number) {
+    return (number - 0) == number && number >= 18 && number.length > 1;
+}
+function validateCVC(number) {
+    return (number - 0) == number && number.length > 2;
+}
+
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
 }
